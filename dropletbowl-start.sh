76d67b363f655ad3b1f9576569ea8921ee64aa38
Joaquin <joacodeviaje@gmail.com>
2024-04-14 11:29:07 -0300

    Install pm2 if necessary

--- dropletbowl-start.sh
+++ dropletbowl-start.sh
@@ -2,6 +2,12 @@
 
 APP_NAME="node-js-sample"
 
+# Check if PM2 is installed
+if ! command -v pm2 >/dev/null 2>&1; then
+    echo "PM2 is not installed. Installing PM2..."
+    npm install pm2@latest -g
+fi
+
 # Check if the application is already running
 if pm2 list | grep -q $APP_NAME; then
     echo "Application $APP_NAME is running. Restarting..."
